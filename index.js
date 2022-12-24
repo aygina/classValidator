e9fd3e939c8faeba979a7999c94cc12d68150630
aygina <aygina.v@gmail.com>
2022-12-24 23:14:46 +0900

    static methods done!

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ class Validator {
         } else return false;
     }
 
-    isDomain(b) {
-        if (b.match(/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/)) {
+    isDomain(a) {
+        if (a.match(/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/)) {
             return true;
         } else return false;
     }
-
 
     isDate(a) {
         if (a.match(/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/)) {
@@ -25,20 +24,29 @@ class Validator {
     }
 }
 
-// class ValidatorStatic {
-//     static isEmail() {
+class ValidatorStatic {
+    static isEmail(a) {
+        if (a.match(/(\<|^)[\w\d._%+-]+@(?:[\w\d-]+\.)+(\w{2,})(\>|$)/i)) {
+            return true;
+        } else return false;
+    }
+    static isDomain(a) {
+        if (a.match(/(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/)) {
+            return true;
+        } else return false;
+    }
+    static isDate(a) {
+        if (a.match(/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/)) {
+            return true;
+        } else return false;
+    }
+    static isPhone(a) {
+        if (a.match(/^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/)) {
+            return true;
+        } else return false;
+    }
+}
 
-//     }
-//     static isDomain() {
-
-//     }
-//     static isDate() {
-
-//     }
-//     static isPhone() {
-
-//     }
-// }
 
 let validator = new Validator();
 console.log(validator.isEmail('alisa@mail.ru'));
@@ -46,7 +54,7 @@ console.log(validator.isDomain('itgirlschool.ru'));
 console.log(validator.isDate('12.05.2021'));
 console.log(validator.isPhone('+7(910)123-45-67'));
 
-// console.log(ValidatorStatic.isEmail('alisa@mail.ru'));
-// console.log(ValidatorStatic.isDomain('itgirlschool.ru'));
-// console.log(ValidatorStatic.isDate('12.05.2021'));
-// console.log(ValidatorStatic.isPhone('+7(910)123-45-67'));
+console.log(ValidatorStatic.isEmail('alisa@mail.ru'));
+console.log(ValidatorStatic.isDomain('itgirlschool.ru'));
+console.log(ValidatorStatic.isDate('12.05.2021'));
+console.log(ValidatorStatic.isPhone('+7(910)123-45-67'));
